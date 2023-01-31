2689cd7010a2695b17428292f350dae7948e86a0
Bartosz Lewandowski <blewandowski.2221@gmail.com>
2023-01-31 22:04:48 +0100

    updated projects page v3

--- components/Molecules/ProjectsPageFilterNav/untils.tsx
+++ components/Molecules/ProjectsPageFilterNav/untils.tsx
@@ -24,19 +24,19 @@ export const projects = [
     description: "LoreLoremLoremLoremLorem Lorem LoremLorem Lorem Loremm",
     href: "",
   },
-  // {
-  //   name: "Bank",
-  //   tech: [
-  //     { name: "React", icon: <FaReact /> },
-  //     { name: "JS", icon: <DiJavascript1 /> },
-  //   ],
-  //   image: {
-  //     src: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     alt: "",
-  //   },
-  //   description: "bank",
-  //   href: "",
-  // },
+  {
+    name: "Bank",
+    tech: [
+      { name: "React", icon: <FaReact /> },
+      { name: "JS", icon: <DiJavascript1 /> },
+    ],
+    image: {
+      src: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "",
+    },
+    description: "bank",
+    href: "",
+  },
 ];
 export const techStack = [
   {
